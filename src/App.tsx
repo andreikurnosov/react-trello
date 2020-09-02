c68a58263cd9cb618448fe69719076c33a000fb9
andreikurnosov <nixdie@yandex.ru>
2020-09-02 13:39:45 +0700

    implemented state management
    - added global state to app via hooks
    - created custom hook for finding item index by id
    - added nanoid package for generating id
    - added feature to autofocus on created input

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
 import { Column } from './Column'
-import { Card } from './Card'
 import { AppContainer } from './styles'
 import { AddNewItem } from './AddNewItem'
+import { useAppState } from './AppStateContext'
 
 function App() {
+  const { state, dispatch } = useAppState()
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column id={list.id} text={list.text} key={list.id} index={i} />
+      ))}
       <AddNewItem
         toggleButtonText="+ Add another list"
-        onAdd={() => console.log}
+        onAdd={(text) => dispatch({ type: 'ADD_LIST', payload: text })}
       />
     </AppContainer>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { AppStateProvider } from './AppStateContext'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppStateProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AppStateProvider>,
   document.getElementById('root')
-);
-
+)
